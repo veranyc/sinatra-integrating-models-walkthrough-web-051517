9d1f5e8470ae21d25e198c89d34cd2af1b998b33
veranyc <veranyc@gmail.com>
2017-06-06 23:11:33 -0400

    Done.

--- spec/integrating_models_sinatra_walkthrough_spec.rb
+++ spec/integrating_models_sinatra_walkthrough_spec.rb
@@ -1,19 +1,19 @@
 describe App do
 
   describe 'GET /' do
-    
+
     it 'sends a 200 status code' do
       get '/'
       expect(last_response.status).to eq(200)
     end
 
-    it 'renders form' do 
+    it 'renders form' do
       visit '/'
       expect(page).to have_selector("form")
       expect(page).to have_field(:user_text)
     end
   end
-  
+
   describe 'TextAnalyzer Class' do 
       let!(:words) { TextAnalyzer.new("mirror mirror on the wall") }
 
@@ -21,14 +21,14 @@ describe App do
       expect(TextAnalyzer.new("hey yall")).to be_an_instance_of(TextAnalyzer)
     end
 
-    it 'can have text' do 
+    it 'can have text' do
       expect(words.text).to eq("mirror mirror on the wall")
     end
 
   end
-  
+
   describe 'POST /' do
-    it "displays string results" do 
+    it "displays string results" do
       visit '/'
 
       fill_in(:user_text, :with => "Green Eggs and Ham")
